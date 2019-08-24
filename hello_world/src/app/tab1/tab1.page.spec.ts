65c5be81e547ad03df5a8af18f764c96f03a08fb
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-08-25 01:35:43 +0900

    feat: add eslint

--- hello_world/src/app/tab1/tab1.page.spec.ts
+++ hello_world/src/app/tab1/tab1.page.spec.ts
@@ -1,24 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { IonicModule } from '@ionic/angular'
 
-import { Tab1Page } from './tab1.page';
+import { Tab1Page } from './tab1.page'
 
 describe('Tab1Page', () => {
-  let component: Tab1Page;
-  let fixture: ComponentFixture<Tab1Page>;
+  let component: Tab1Page
+  let fixture: ComponentFixture<Tab1Page>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [Tab1Page],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+      imports: [IonicModule.forRoot()],
+    }).compileComponents()
 
-    fixture = TestBed.createComponent(Tab1Page);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+    fixture = TestBed.createComponent(Tab1Page)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  }))
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
