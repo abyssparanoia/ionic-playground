030a28fdb02a9777c28823bff07f66b3824ec211
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-08-25 20:42:59 +0900

    feat: add tabs modules

---
+++ connpass/src/app/bookmark/bookmark.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-bookmark',
+  templateUrl: './bookmark.page.html',
+  styleUrls: ['./bookmark.page.scss'],
+})
+export class BookmarkPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
