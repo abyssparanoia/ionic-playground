ad06dee3c24abd6d78d369f623a8d96a80b0fef8
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-08-25 12:29:10 +0900

    feat: impl directive

---
+++ hello_world/src/app/directives/highlight.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, ElementRef, Input } from '@angular/core'
+
+@Directive({
+  selector: '[appHighlight]',
+})
+export class HighlightDirective {
+  @Input()
+  set appHighlight(condition: any) {
+    this.el.nativeElement.style.background = condition ? 'yellow' : null
+  }
+
+  constructor(private el: ElementRef) {}
+}
--- hello_world/src/app/modules/app-common/app-common.module.ts
+++ hello_world/src/app/modules/app-common/app-common.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { IonicModule } from '@ionic/angular'
 import { CommonModule } from '@angular/common'
 import { UserComponent } from '../../components/user/user.component'
+import { HighlightDirective } from '../../directives/highlight.directive'
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, HighlightDirective],
   imports: [CommonModule, IonicModule],
-  exports: [UserComponent],
+  exports: [UserComponent, HighlightDirective],
 })
 export class AppCommonModule {}
