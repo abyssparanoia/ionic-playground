6124671e33ce7b21772a986a156fa941db4331b6
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-08-25 18:29:16 +0900

    feat: add linter

---
+++ connpass/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    es6: true,
+    jasmine: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:angular/johnpapa',
+    'plugin:jasmine/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: {
+      tsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'jasmine'],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': ['error', { allow: ['info', 'error'] }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
